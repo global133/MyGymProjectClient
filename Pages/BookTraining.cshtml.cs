dd10af93159da8642b2cf9b46409256f3454728c
global133 <sergej.chuev.98@mail.ru>
2025-06-24 16:33:23 +0300

    added display schedule for every trainer

--- Pages/BookTraining.cshtml.cs
+++ Pages/BookTraining.cshtml.cs
@@ -14,6 +14,8 @@ namespace MyGymProject.Client.Pages
         [BindProperty]
         public int SelectedTrainingId { get; set; }
 
+        [BindProperty(SupportsGet = true)]
+        public int WeekOffset { get; set; } = 0;
         public TrainerReadDto Trainer { get; set; }
 
         public List<string> TimeSlots { get; set; } = new()
@@ -21,7 +23,10 @@ namespace MyGymProject.Client.Pages
             "08:00", "10:00", "12:00", "14:00", "16:00", "18:00"
         };
 
-        public Dictionary<DateTime, Dictionary<string, TrainingResponseDTO>> SlotMatrix { get; set; } = new();
+        public Dictionary<DayOfWeek, Dictionary<string, TrainingResponseDTO>> WeeklySchedule { get; set; } = new();
+        public DateTime StartOfWeek { get; private set; }
+        public DateTime EndOfWeek { get; private set; }
+
         public BookTrainingModel(
             IHttpClientFactory httpClientFactory, 
             IHttpContextAccessor contextAccessor) : base(httpClientFactory, contextAccessor)
@@ -33,29 +38,38 @@ namespace MyGymProject.Client.Pages
 
             var schedule = await this._httpClient.GetFromJsonAsync<List<TrainingResponseDTO>>($"http://localhost:5155/api/trainings/trainer/{TrainerId}");
 
-            var days = schedule
-                .Select(t => t.Time.Date)
-                .Distinct()
-                .OrderBy(d => d)
-                .Take(7); // показываем 7 дней
+            var currentDate = DateTime.Today;
+            StartOfWeek = currentDate.AddDays(-(int)currentDate.DayOfWeek + (int)DayOfWeek.Monday)
+                          .AddDays(WeekOffset * 7);
+            EndOfWeek = StartOfWeek.AddDays(6);
 
-            foreach (var day in days)
+            var daysOfWeek = Enum.GetValues(typeof(DayOfWeek)).Cast<DayOfWeek>();
+            foreach (var day in daysOfWeek)
             {
-                SlotMatrix[day] = new Dictionary<string, TrainingResponseDTO>();
-                foreach (var slot in schedule.Where(t => t.Time.Date == day))
+                WeeklySchedule[day] = new Dictionary<string, TrainingResponseDTO>();
+                foreach (var timeSlot in TimeSlots)
                 {
-                    var time = slot.Time.ToString("HH:mm");
-                    SlotMatrix[day][time] = slot;
+                    WeeklySchedule[day][timeSlot] = null; 
                 }
             }
 
+            foreach (var training in schedule.Where(t => t.Time.Date >= StartOfWeek && t.Time.Date <= EndOfWeek))
+            {
+                var dayOfWeek = training.Time.DayOfWeek;
+                var time = training.Time.ToString("HH:mm");
+
+                if (WeeklySchedule.ContainsKey(dayOfWeek) && WeeklySchedule[dayOfWeek].ContainsKey(time))
+                {
+                    WeeklySchedule[dayOfWeek][time] = training;
+                }
+            }
             return Page();
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
             var clientId = await LoadClientAsync();
-            var response = await this._httpClient.PostAsJsonAsync($"http://localhost:5155/api/trainings/{TrainerId}/clients/{clientId}", new { });
+            var response = await this._httpClient.PostAsJsonAsync($"http://localhost:5155/api/trainings/{SelectedTrainingId}/clients/{clientId}", new { });
 
             if (!response.IsSuccessStatusCode)
             {
@@ -64,7 +78,7 @@ namespace MyGymProject.Client.Pages
             }
 
             TempData["Success"] = "Вы успешно записались!";
-            return RedirectToPage("/ClientSchedule"); // или другую страницу
+            return RedirectToPage("/ClientSchedule"); 
         }
     }
 }
