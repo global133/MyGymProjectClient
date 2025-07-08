906c64988d92266381ec8d2d1a2f696ed3e8d78d
global133 <sergej.chuev.98@mail.ru>
2025-07-08 15:20:04 +0300

    Added caching and page to choose session on workout

--- Pages/BookTraining.cshtml.cs
+++ Pages/BookTraining.cshtml.cs
@@ -3,37 +3,43 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Caching.Memory;
 using MyGymProject.Server.DTOs.Trainer;
 using MyGymProject.Server.DTOs.Training;
+using MyGymProject.Server.DTOs.TrainingSession;
 using MyGymProject.Server.Models;
+
 
 namespace MyGymProject.Client.Pages
 {
     public class BookTrainingModel : AuthorizedPageModel
     {
         [BindProperty(SupportsGet = true)]
+        public int TrainingId { get; set; }
+        public TrainingResponseDTO Training { get; set; }
+
+
+        [BindProperty(SupportsGet = true)]
         public int TrainerId { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public TrainerReadDto Trainer { get; set; }
+
 
         [BindProperty(SupportsGet = true)]
         public int WeekOffset { get; set; } = 0;
 
-        [BindProperty(SupportsGet = true)]
-        public string TrainingName { get; set; }
-
-        public TrainerReadDto Trainer { get; set; }
-        public List<string> TimeSlots { get; set; } = new()
-        {
-            "08:00", "10:00", "12:00", "14:00", "16:00", "18:00"
-        };
-
-        public Dictionary<DayOfWeek, Dictionary<string, TrainingResponseDTO>> WeeklySchedule { get; set; } = new();
+        public List<DateTime> DaysOfWeek { get; set; } = new();
+        public List<TrainingSessionReadDto> UpcomingSessions { get; set; } = new();
         public DateTime StartOfWeek { get; private set; }
         public DateTime EndOfWeek { get; private set; }
 
         private readonly IMemoryCache _cache;
+
         public BookTrainingModel(
-            IHttpClientFactory httpClientFactory, 
-            IHttpContextAccessor contextAccessor, 
+            IHttpClientFactory httpClientFactory,
+            IHttpContextAccessor contextAccessor,
             IMemoryCache memoryCache) : base(httpClientFactory, contextAccessor)
-        { _cache = memoryCache; }
+        {
+            _cache = memoryCache;
+        }
 
         public async Task<IActionResult> OnGetAsync()
         {
@@ -42,50 +48,44 @@ namespace MyGymProject.Client.Pages
                 trainer = await _httpClient.GetFromJsonAsync<TrainerReadDto>($"http://localhost:5155/api/Trainers/{TrainerId}");
                 _cache.Set($"trainer_{TrainerId}", trainer, TimeSpan.FromMinutes(10));
             }
-
-            var schedule = await this._httpClient.GetFromJsonAsync<List<TrainingResponseDTO>>($"http://localhost:5155/api/trainings/{TrainingName}trainer/{TrainerId}");
+            Trainer = trainer;
 
             var currentDate = DateTime.Today;
             StartOfWeek = currentDate.AddDays(-(int)currentDate.DayOfWeek + (int)DayOfWeek.Monday)
                           .AddDays(WeekOffset * 7);
             EndOfWeek = StartOfWeek.AddDays(6);
 
-            var daysOfWeek = Enum.GetValues(typeof(DayOfWeek)).Cast<DayOfWeek>();
-            foreach (var day in daysOfWeek)
-            {
-                WeeklySchedule[day] = new Dictionary<string, TrainingResponseDTO>();
-                foreach (var timeSlot in TimeSlots)
-                {
-                    WeeklySchedule[day][timeSlot] = null; 
-                }
-            }
+            DaysOfWeek = Enumerable.Range(0, 7)
+                .Select(i => StartOfWeek.AddDays(i))
+                .ToList();
 
-            foreach (var training in schedule.Where(t => t.Time.Date >= StartOfWeek && t.Time.Date <= EndOfWeek))
-            {
-                var dayOfWeek = training.Time.DayOfWeek;
-                var time = training.Time.ToString("HH:mm");
+            Training = await _httpClient.GetFromJsonAsync<TrainingResponseDTO>($"http://localhost:5155/api/trainings/{TrainingId}");
+            UpcomingSessions = await _httpClient.GetFromJsonAsync<List<TrainingSessionReadDto>>($"http://localhost:5155/api/trainings/{TrainingId}/sessions/upcoming");
 
-                if (WeeklySchedule.ContainsKey(dayOfWeek) && WeeklySchedule[dayOfWeek].ContainsKey(time))
-                {
-                    WeeklySchedule[dayOfWeek][time] = training;
-                }
-            }
+            // Фильтрация по текущей неделе
+            UpcomingSessions = UpcomingSessions?
+                .Where(s => s.StartTime >= StartOfWeek && s.StartTime <= EndOfWeek)
+                .OrderBy(s => s.StartTime)
+                .ToList() ?? new List<TrainingSessionReadDto>();
+
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(int sessionId)
         {
             var client = await LoadClientAsync();
-            var response = await this._httpClient.PostAsJsonAsync($"http://localhost:5155/api/trainings/{TrainerId}/clients/{client.Id}", new { });
+            var response = await _httpClient.PostAsJsonAsync(
+                $"api/trainings/{sessionId}/clients/{client.Id}",
+                new { });
 
             if (!response.IsSuccessStatusCode)
             {
-                TempData["Error"] = "Не удалось записаться.";
-                return RedirectToPage(new { TrainerId });
+                TempData["Error"] = "Не удалось записаться на тренировку.";
+                return RedirectToPage(new { TrainerId, WeekOffset });
             }
 
-            TempData["Success"] = "Вы успешно записались!";
-            return RedirectToPage("/ClientSchedule"); 
+            TempData["Success"] = "Вы успешно записались на тренировку!";
+            return RedirectToPage("/ClientSchedule");
         }
     }
 }
