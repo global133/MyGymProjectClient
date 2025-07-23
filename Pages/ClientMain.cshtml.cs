cba24bbaae4ea214d535f343bf52f4b6c54a71d7
global133 <sergej.chuev.98@mail.ru>
2025-07-23 09:59:28 +0300

    added display count workouts in personal account

--- Pages/ClientMain.cshtml.cs
+++ Pages/ClientMain.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using MyGymProject.Server.DTOs.Client;
+using MyGymProject.Server.DTOs.TrainingSession;
 using System.Net.Http.Headers;
 
 namespace MyGymProject.Client.Pages
@@ -9,6 +10,9 @@ namespace MyGymProject.Client.Pages
 
         [BindProperty]
         public ClientReadDto client { get; set; }
+
+        [BindProperty]
+        public List<TrainingSessionReadDto> trainingSessions { get; set; }
 
         [BindProperty]
 
@@ -34,6 +38,16 @@ namespace MyGymProject.Client.Pages
                 return RedirectToPage("/LoginPage");
 
             client = clientData;
+
+            var response = await _httpClient.GetAsync($"http://localhost:5155/api/Clients/schedule{client.Id}");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new HttpRequestException($"Ошибка: {response.StatusCode}");
+            }
+
+            trainingSessions = await response.Content.ReadFromJsonAsync<List<TrainingSessionReadDto>>();
+            
             return Page();
         }
 
