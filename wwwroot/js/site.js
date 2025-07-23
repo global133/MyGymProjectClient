e59f1947365de9b03065888a561bf33118f6e717
global133 <sergej.chuev.98@mail.ru>
2025-07-23 15:28:40 +0300

    function ReloadPage was moved in site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function ReloadPage(id) {
+    setTimeout(() => {
+        const msg = document.getElementById(id);
+        if (msg) location.reload();
+    }, 2000);
+};
