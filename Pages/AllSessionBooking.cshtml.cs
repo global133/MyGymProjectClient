899560822d23c6956e4061457b9f2877af4c1aa7
global133 <sergej.chuev.98@mail.ru>
2025-08-04 16:54:23 +0300

    add getting all sessions

--- Pages/AllSessionBooking.cshtml.cs
+++ Pages/AllSessionBooking.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MyGymProject.Server.DTOs.Client;
+using MyGymProject.Server.DTOs.TrainingSession;
 using System.Threading.Tasks;
 
 namespace MyGymProject.Client.Pages
@@ -11,6 +12,9 @@ namespace MyGymProject.Client.Pages
 
         public ClientReadDto Client { get; set; }
 
+        [BindProperty]
+
+        public List<TrainingSessionReadDto> TrainingSessions { get; set; } = new List<TrainingSessionReadDto>();
         public AllSessionBookingModel(
             IHttpClientFactory httpClientFactory, 
             IHttpContextAccessor contextAccessor, 
@@ -23,8 +27,8 @@ namespace MyGymProject.Client.Pages
             if (clientData == null)
                 return RedirectToPage("/LoginPage");
 
-            
-            
+
+            TrainingSessions = await this._httpClient.GetFromJsonAsync<List<TrainingSessionReadDto>>($"{_apiBaseUrl}/trainings/all");
 
             return Page();
         }
